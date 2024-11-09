3e2cc7a9042d22e14123fe5c39a1128e0218275c
De8B8T <digglealsodiggle@gmail.com>
2024-11-09 23:15:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ document.querySelectorAll('.faq-item').forEach(item => {
     answer.classList.toggle('active');
   });
 });
+
+const banner = document.querySelector('.banner-follow');
+document.addEventListener('mousemove', (e) => {
+  const { clientX: x, clientY: y } = e;
+  const moveX = (x / window.innerWidth - 0.5) * 30;
+  const moveY = (y / window.innerHeight - 0.5) * 30;
+  banner.style.transform = `translate(-50%, -50%) rotateX(${moveY}deg) rotateY(${moveX}deg)`;
+});
+
+function toggleContent(item) {
+  const content = item.querySelector('.changelog-content');
+  content.classList.toggle('active');
+  item.classList.toggle('active');
+}
